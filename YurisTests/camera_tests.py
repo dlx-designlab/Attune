0eba2cdfd61169a262f4f6e642cd927479e0cece
Yuri Klebanov <yurikleb@gmail.com>
2019-12-18 21:47:12 +0900

    testing nail-fold edge detection

--- YurisTests/camera_tests.py
+++ YurisTests/camera_tests.py
@@ -1,17 +1,29 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture(2)
+cap = cv2.VideoCapture(0)
+print('is cap opened: ', cap.isOpened())
+
+ret, frame = cap.read()
+print(ret)
+
+# cap = cv2.imread('./nail.png')
+# frame_HSV = cv2.cvtColor(frame, cv2.COLOR_RGB2HSV)
+# cv2.imshow('nail', frame_HSV)
+# cv2.waitKey(0)
 
 while(True):
     # Capture frame-by-frame
+    if ret is True:
+        # Our operations on the frame come here
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # Display the resulting frame
+        cv2.imshow('frame', gray)
+
     ret, frame = cap.read()
+    print(ret)
 
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # Display the resulting frame
-    cv2.imshow('frame',gray)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
