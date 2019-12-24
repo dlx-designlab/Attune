0850e65b91daf2b25c79ddc1b9fc96094fee7ddc
Yuri Klebanov <yurikleb@gmail.com>
2019-12-24 20:22:58 +0900

    added blur detection

--- YurisTests/camera_tests.py
+++ YurisTests/camera_tests.py
@@ -2,10 +2,17 @@ import numpy as np
 import cv2
 
 cap = cv2.VideoCapture(0)
+
 print('is cap opened: ', cap.isOpened())
 
-ret, frame = cap.read()
-print(ret)
+# print(cap.get(cv2.CAP_PROP_AUTO_WB))
+# print(cap.get(cv2.CAP_PROP_WB_TEMPERATURE))
+# print(cap.get(cv2.CAP_PROP_FOCUS))
+# print(cap.get(cv2.CAP_PROP_WB_TEMPERATURE))
+
+# cap.set(cv2.CAP_PROP_AUTOFOCUS, 1)
+# cap.set(cv2.CAP_PROP_FOCUS, 100)
+# cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 
 # cap = cv2.imread('./nail.png')
 # frame_HSV = cv2.cvtColor(frame, cv2.COLOR_RGB2HSV)
@@ -13,16 +20,24 @@ print(ret)
 # cv2.waitKey(0)
 
 while(True):
+
     # Capture frame-by-frame
+    ret, frame = cap.read()
+
     if ret is True:
         # Our operations on the frame come here
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        # Display the resulting frame
-        cv2.imshow('frame', gray)
+        # Check Blur Amount
+        fm = cv2.Laplacian(gray, cv2.CV_64F).var()
 
-    ret, frame = cap.read()
-    print(ret)
+        # Add Blur Value to the frame
+        cv2.putText(frame, "Sharpness Index: {:.2f}".format(fm), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 3)
+
+        # Display the resulting frame
+        cv2.imshow('frame', frame)
+
+    # print(ret)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
